7e0968f03183e1650a9ee70d8c3360dc1074c95b
Mauricio Carvalho <developer.mauricio1@gmail.com>
2022-12-21 18:53:29 -0300

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+}
