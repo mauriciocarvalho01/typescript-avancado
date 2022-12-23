eacd0839ce184c84ff8e3049a645fdf796b97e67
Mauricio Carvalho <developer.mauricio1@gmail.com>
2022-12-23 16:52:32 -0300

    Ensure AccessToken expires in 30 min

--- tests/domain/models/access-token.spec.ts
+++ tests/domain/models/access-token.spec.ts
@@ -6,8 +6,7 @@ describe('AccessToken', () => {
     expect(sut).toEqual({ value: 'any_value' })
   })
 
-  it('Should expire in ', () => {
-    const sut = new AccessToken('any_value')
-    expect(sut).toEqual({ value: 'any_value' })
+  it('Should expire in 1800000 ms', () => {
+    expect(AccessToken.expirationInMs).toEqual(1800000)
   })
 })
