0a1466314029d7daea4da4c381c6fd2255ea0936
Mauricio Carvalho <developer.mauricio1@gmail.com>
2022-12-23 17:54:40 -0300

    Make pattern triple A in tests file

--- tests/data/use-cases/google-authentication.spec.ts
+++ tests/data/use-cases/google-authentication.spec.ts
@@ -28,8 +28,10 @@ describe('GoogleAuthenticationUseCase', () => {
   let googleApi: MockProxy<LoadGoogleUserApi>
   let userAccountRepository: MockProxy<LoadUserAccountRepository & SaveGoogleAccountRepository>
   let sut: GoogleAuthenticationUseCase
-  const token = 'any_token'
-  beforeEach(() => {
+  let token: string
+
+  beforeAll(() => {
+    token = 'any_token'
     googleApi = mock()
     googleApi.loadUser.mockResolvedValue({
       name: 'any_google_name',
@@ -37,27 +39,34 @@ describe('GoogleAuthenticationUseCase', () => {
       googleId: 'any_google_id'
     })
     userAccountRepository = mock()
-    crypto = mock()
-    crypto.generateToken.mockResolvedValue('any_generated_token')
     userAccountRepository.load.mockResolvedValue(undefined)
     userAccountRepository.saveWithGoogle.mockResolvedValue({ id: 'any_account_id' })
+    crypto = mock()
+    crypto.generateToken.mockResolvedValue('any_generated_token')
+  })
+
+  beforeEach(() => {
     sut = new GoogleAuthenticationUseCase(googleApi, userAccountRepository, crypto)
   })
 
   it('Should call LoadGoogleUserApi with correct params', async () => {
     await sut.perform({ token: 'any_token' })
+
     expect(googleApi.loadUser).toHaveBeenCalledWith({ token })
     expect(googleApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('Should return AuthenticationError when LoadGoogleUserApi returns undefined', async () => {
     googleApi.loadUser.mockResolvedValueOnce(undefined)
+
     const authResult = await sut.perform({ token })
+
     expect(authResult).toEqual(new AuthenticationError())
   })
 
   it('Should call LoadUserAccountRepository when LoadGoogleUserApi returns data', async () => {
     await sut.perform({ token })
+
     expect(userAccountRepository.load).toHaveBeenCalledWith({ email: 'any_google_email' })
     expect(userAccountRepository.load).toHaveBeenCalledTimes(1)
   })
@@ -79,6 +88,7 @@ describe('GoogleAuthenticationUseCase', () => {
 
   it('Should call TokenGenerator with corret params', async () => {
     await sut.perform({ token })
+
     expect(crypto.generateToken).toHaveBeenCalledWith({
       key: 'any_account_id',
       expirationInMs: AccessToken.expirationInMs
@@ -94,25 +104,33 @@ describe('GoogleAuthenticationUseCase', () => {
 
   it('Should rethrow if LoadGoogleUserApi throws', async () => {
     googleApi.loadUser.mockRejectedValueOnce(new Error('google_error'))
+
     const promise = sut.perform({ token })
+
     await expect(promise).rejects.toThrow(new Error('google_error'))
   })
 
   it('Should rethrow if LoadUserAccountRepository throws', async () => {
     userAccountRepository.load.mockRejectedValueOnce(new Error('account_error'))
+
     const promise = sut.perform({ token })
+
     await expect(promise).rejects.toThrow(new Error('account_error'))
   })
 
   it('Should rethrow if SaveGoogleAccountRepository throws', async () => {
     userAccountRepository.saveWithGoogle.mockRejectedValueOnce(new Error('save_error'))
+
     const promise = sut.perform({ token })
+
     await expect(promise).rejects.toThrow(new Error('save_error'))
   })
 
   it('Should rethrow if TokenGenerator throws', async () => {
     crypto.generateToken.mockRejectedValueOnce(new Error('token_error'))
+
     const promise = sut.perform({ token })
+
     await expect(promise).rejects.toThrow(new Error('token_error'))
   })
 })
