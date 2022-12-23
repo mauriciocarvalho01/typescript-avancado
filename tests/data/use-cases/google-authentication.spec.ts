757d13a77a1094230f41176f05700712aa64899e
Mauricio Carvalho <developer.mauricio1@gmail.com>
2022-12-23 09:48:15 -0300

    Change sut creation strategy to use beforeEach

--- tests/data/use-cases/google-authentication.spec.ts
+++ tests/data/use-cases/google-authentication.spec.ts
@@ -1,21 +1,39 @@
 import { AuthenticationError } from '@/domain/errors'
 import { GoogleAuthenticationUseCase } from '@/data/use-cases/google-authentication'
 import { LoadGoogleUserApi } from '@/data/contracts/apis'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+// Factory Patthern
+// type SuTypes = {
+//   sut: GoogleAuthenticationUseCase
+//   loadGoogleUserApi: MockProxy<LoadGoogleUserApi>
+// }
+
+// const makeSut = (): SuTypes => {
+//   const loadGoogleUserApi = mock<LoadGoogleUserApi>()
+//   const sut = new GoogleAuthenticationUseCase(loadGoogleUserApi)
+//   return {
+//     sut, loadGoogleUserApi
+//   }
+// }
 
 describe('GoogleAuthenticationUseCase', () => {
+  let loadGoogleUserApi: MockProxy<LoadGoogleUserApi>
+  let sut: GoogleAuthenticationUseCase
+
+  beforeEach(() => {
+    loadGoogleUserApi = mock()
+    sut = new GoogleAuthenticationUseCase(loadGoogleUserApi)
+  })
+
   it('Should call LoadGoogleUserApi with correct params', async () => {
-    const loadGoogleUserApi = mock<LoadGoogleUserApi>()
-    const sut = new GoogleAuthenticationUseCase(loadGoogleUserApi)
     await sut.perform({ token: 'any_token' })
     expect(loadGoogleUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
     expect(loadGoogleUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('Should return AuthenticationError when LoadGoogleUserApi returns undefined', async () => {
-    const loadGoogleUserApi = mock<LoadGoogleUserApi>()
     loadGoogleUserApi.loadUser.mockResolvedValueOnce(undefined)
-    const sut = new GoogleAuthenticationUseCase(loadGoogleUserApi)
     const authResult = await sut.perform({ token: 'any_token' })
     expect(authResult).toEqual(new AuthenticationError())
   })
