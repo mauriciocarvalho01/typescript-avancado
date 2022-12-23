8e3652d6d427805e2a2964dc5cd2a0e11528343b
Mauricio Carvalho <developer.mauricio1@gmail.com>
2022-12-23 16:44:49 -0300

    Ensure AccessToken has a value

---
+++ tests/domain/models/access-token.spec.ts
@@ -0,0 +1,13 @@
+import { AccessToken } from '@/domain/models'
+
+describe('AccessToken', () => {
+  it('Should create with a value', () => {
+    const sut = new AccessToken('any_value')
+    expect(sut).toEqual({ value: 'any_value' })
+  })
+
+  it('Should expire in ', () => {
+    const sut = new AccessToken('any_value')
+    expect(sut).toEqual({ value: 'any_value' })
+  })
+})
