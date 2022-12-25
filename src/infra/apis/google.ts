e40b2ba7755f4f8b91c04e5280e14c5b6e5e0368
Mauricio Carvalho <developer.mauricio1@gmail.com>
2022-12-25 08:35:22 -0300

    Ensure GoogleApi implements interface LoadGoogleUserApi

--- src/infra/apis/google.ts
+++ src/infra/apis/google.ts
@@ -1,7 +1,7 @@
 import { ProviderClient } from '@/infra/apis'
 import { LoadGoogleUserApi } from '@/data/contracts/apis'
 
-export class GoogleApi {
+export class GoogleApi implements LoadGoogleUserApi {
   constructor (private readonly googleClient: ProviderClient, private readonly clientId: string) { }
   async loadUser (params: LoadGoogleUserApi.Input): Promise<LoadGoogleUserApi.Output> {
     return await this.googleClient.verifyIdToken({ token: params.token, clientId: this.clientId })
