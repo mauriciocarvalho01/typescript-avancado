87ce3dff1e31124214de3fa11a7daedc4153d4a1
Mauricio Carvalho <developer.mauricio1@gmail.com>
2022-12-26 18:38:03 -0300

    Ensure GoogleLoginController returns 400 if token is undefined

--- tests/application/login/google-login.spec.ts
+++ tests/application/login/google-login.spec.ts
@@ -10,6 +10,42 @@ describe('GoogleLoginController', () => {
       }
     )
   })
+
+  it('Should return if token is null ', async () => {
+    const sut = new GoogleLoginController()
+
+    const httpResponse = await sut.handle({ token: null })
+    expect(httpResponse).toEqual(
+      {
+        statusCode: 400,
+        data: new Error('The field token is required')
+      }
+    )
+  })
+
+  it('Should return if token is undefined ', async () => {
+    const sut = new GoogleLoginController()
+
+    const httpResponse = await sut.handle({ token: undefined })
+    expect(httpResponse).toEqual(
+      {
+        statusCode: 400,
+        data: new Error('The field token is required')
+      }
+    )
+  })
+
+  it('Should call GoogleAureturn if token is null ', async () => {
+    const sut = new GoogleLoginController()
+
+    const httpResponse = await sut.handle({ token: null })
+    expect(httpResponse).toEqual(
+      {
+        statusCode: 400,
+        data: new Error('The field token is required')
+      }
+    )
+  })
 })
 
 type httpResponse = {
