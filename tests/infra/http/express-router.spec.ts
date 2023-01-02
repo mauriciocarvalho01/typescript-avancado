7077576dd3f2b4c47441c4d4700e643856fc7ae4
Mauricio Carvalho <developer.mauricio1@gmail.com>
2023-01-02 14:28:28 -0300

    Isolate SUT creation

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,16 +1,29 @@
 import { Request, Response } from 'express'
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { Controller } from '@/application/controllers'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('ExpressRouter', () => {
+  let req: Request
+  let res: Response
+  let controller: MockProxy<Controller>
+  let sut: ExpressRouter
+  beforeEach(() => {
+    req = getMockReq({ body: { any: 'any' } })
+    res = getMockRes().res
+    controller = mock()
+    sut = new ExpressRouter(controller)
+  })
   it('should call handle with correct request', async () => {
-    const req = getMockReq({ body: { any: 'any' } })
-    const { res } = getMockRes()
-    const controller = mock<Controller>()
-    const sut = new ExpressRouter(controller)
     await sut.adapter(req, res)
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+  })
+
+  it('should call handle with empty request', async () => {
+    req = getMockReq({ body: undefined })
+    const sut = new ExpressRouter(controller)
+    await sut.adapter(req, res)
+    expect(controller.handle).toHaveBeenCalledWith({})
   })
 })
 
